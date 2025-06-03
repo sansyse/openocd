6d9e1069e3a4c250e39aa3f91cf4795f4e1a9365
Sten <sten.sandring@sansys-electronic.com>
2025-06-03 14:52:12 +0200

    STM32H7 implemented.

--- src/flash/nor/stm32h5x.c
+++ src/flash/nor/stm32h5x.c
@@ -188,7 +188,7 @@ static int Write_U5(struct flash_bank* bank, const uint8_t* buffer, uint32_t dst
 	assert(bank != NULL);
 	assert((dstOffs + nBytes) <= bank->size);
 	uint32_t addr						= (dstOffs + bank->base);
-	assert((addr & 127) == 0);
+	assert((addr & (128/8-1)) == 0);
 	assert(bank->chip_width == 128/8);
 	struct STM32H5xxPrv_s* prv			= (struct STM32H5xxPrv_s*)(bank->driver_priv);
 	assert(prv != NULL);
@@ -274,6 +274,7 @@ static int Wait4EOP_H5(struct flash_bank* bank, unsigned int timeout) {
 		uint32_t val;
 		if ((ret = target_read_u32(bank->target, H5_NSSR, &val)) == ERROR_OK) {
 			if ((val & 0x00fe0000) != 0) {
+				LOG_ERROR("Flash error 0x%08x", val);
 				ret		= ERROR_FAIL;
 				break;
 			}
@@ -371,7 +372,7 @@ static int Write_H5(struct flash_bank* bank, const uint8_t* buffer, uint32_t dst
 	assert(bank != NULL);
 	assert((dstOffs + nBytes) <= bank->size);
 	uint32_t addr						= (dstOffs + bank->base);
-	assert((addr & 127) == 0);
+	assert((addr & (128/8-1)) == 0);
 	assert(bank->chip_width == 128/8);
 	struct STM32H5xxPrv_s* prv			= (struct STM32H5xxPrv_s*)(bank->driver_priv);
 	assert(prv != NULL);
@@ -451,20 +452,30 @@ static int Unlock_H7(struct flash_bank* bank, unsigned int bankNum) {
 	return ret;
 }
 
+static int Check4EOP_H7(struct flash_bank* bank, unsigned int bankNum) {
+	int ret;
+	uint32_t val;
+	if ((ret = target_read_u32(bank->target, H7_SR(bankNum), &val)) == ERROR_OK) {
+		if ((val & 0x07ee0000) != 0) {
+			LOG_ERROR("Flash error 0x%08x", val);
+			ret		= ERROR_FAIL;
+		}
+		else if ((val & 0x0000000b) == 0) {
+			ret		= ERROR_OK;
+		}
+		else {
+			ret		= ERROR_WAIT;
+		}
+	}
+	return ret;
+}
+
 static int Wait4EOP_H7(struct flash_bank* bank, unsigned int bankNum, unsigned int timeout) {
 	int ret		= ERROR_FAIL;
 	while (0 < timeout) {
 		alive_sleep(1);
-		uint32_t val;
-		if ((ret = target_read_u32(bank->target, H7_SR(bankNum), &val)) == ERROR_OK) {
-			if ((val & 0x00ff0000) != 0) {
-				ret		= ERROR_FAIL;
-				break;
-			}
-			else if ((val & 0x0000000b) == 0) {
-				ret		= ERROR_OK;
-				break;
-			}
+		if ((ret = Check4EOP_H7(bank, bankNum)) != ERROR_WAIT) {
+			break;
 		}
 		timeout--;
 		ret			= ERROR_FAIL;
@@ -486,32 +497,216 @@ static int CheckNoOp_H7(struct flash_bank* bank, unsigned int bankNum) {
 }
 
 static int ClearErrorFlags_H7(struct flash_bank* bank, unsigned int bankNum) {
-	return target_write_u32(bank->target, H7_CCR(bankNum), 0x00ff0000);
+	return target_write_u32(bank->target, H7_CCR(bankNum), 0x0fef0000);
 }
 
 static int MassErase_H7(struct flash_bank* bank) {
 	int ret								= ERROR_FAIL;
-	for (unsigned int bankNum = 0; bankNum < 2; bankNum++) {
-		if ((ret = CheckNoOp_H7(bank, bankNum)) == ERROR_OK
-			&& (ret = ClearErrorFlags_H7(bank, bankNum)) == ERROR_OK
-			&& (ret = Unlock_H7(bank, bankNum)) == ERROR_OK) {
-			/* Start mass erase */
-			if ((ret = target_write_u32(bank->target, H7_CR(bankNum), 0x00008020)) == ERROR_OK) {
-				ret			= Wait4EOP_H7(bank, bankNum, 3000);
-
-				/* Reset MER bit */
-				target_write_u32(bank->target, H7_CR(bankNum), 0x00000000);
+	if ((ret = CheckNoOp_H7(bank, 0)) == ERROR_OK
+		&& (ret = CheckNoOp_H7(bank, 1)) == ERROR_OK
+		&& (ret = ClearErrorFlags_H7(bank, 0)) == ERROR_OK
+		&& (ret = ClearErrorFlags_H7(bank, 1)) == ERROR_OK
+		&& (ret = Unlock_H7(bank, 0)) == ERROR_OK
+		&& (ret = Unlock_H7(bank, 1)) == ERROR_OK) {
+		/* Start mass erase */
+		if ((ret = target_write_u32(bank->target, H7_CR(0), 0x00000088)) == ERROR_OK
+			&& (ret = target_write_u32(bank->target, H7_CR(1), 0x00000088)) == ERROR_OK) {
+			if ((ret = Wait4EOP_H7(bank, 0, 10000)) != ERROR_OK) {
+				LOG_ERROR("End of operation failed!");
+			}
+			else if ((ret = Wait4EOP_H7(bank, 1, 300)) != ERROR_OK) {
+				LOG_ERROR("End of operation failed!");
 			}
 
-			/* Lock flash */
-			Lock_H7(bank, bankNum);
+			/* Reset MER bit */
+			target_write_u32(bank->target, H7_CR(0), 0x00000000);
+			target_write_u32(bank->target, H7_CR(1), 0x00000000);
 		}
+
+		/* Lock flash */
+		Lock_H7(bank, 0);
+		Lock_H7(bank, 1);
 	}
 	return ret;
 }
 
 static int Erase_H7(struct flash_bank* bank, unsigned int first, unsigned int last) {
-	return 0;
+	LOG_ERROR("Implement me!!!");
+	return -1;
+}
+
+#if 0
+static int Write_H7_banked(struct flash_bank* bank, const uint8_t* buffer, uint32_t addr, uint32_t nBytes, unsigned int bankNum) {
+	struct STM32H5xxPrv_s* prv			= (struct STM32H5xxPrv_s*)(bank->driver_priv);
+	assert(prv != NULL);
+	const struct STM32H5xxDef_s* def	= prv->Def;
+	assert(def != NULL);
+	int ret								= ERROR_FAIL;
+
+	if ((ret = CheckNoOp_H7(bank, bankNum)) == ERROR_OK
+		&& (ret = ClearErrorFlags_H7(bank, bankNum)) == ERROR_OK
+		&& (ret = Unlock_H7(bank, bankNum)) == ERROR_OK) {
+		LOG_INFO("Programming %u bytes at 0x%08x", nBytes, addr);
+		/* Start programming */
+		if ((ret = target_write_u32(bank->target, H7_CR(bankNum), 0x00000002)) == ERROR_OK) {
+			uint8_t		data[256/8];
+			while (0 < nBytes && ret == ERROR_OK) {
+				size_t bi	= 0;
+				while (bi < sizeof(data) && 0 < nBytes) {
+					data[bi++]		= *(buffer++);
+					nBytes--;
+				}
+				while (bi < sizeof(data)) {
+					data[bi++]		= 0xff;
+				}
+				if ((ret = target_write_memory(bank->target, addr, 32/8, 256/32, data)) != ERROR_OK) {
+					LOG_ERROR("Write operation failed! 0x%08x", addr);
+					break;
+				}
+				addr		+= 256/8;
+				if ((ret = Wait4EOP_H7(bank, bankNum, 700)) != ERROR_OK) {
+					LOG_ERROR("End of operation failed!");
+				}
+			}
+
+			/* Reset PG bit */
+			target_write_u32(bank->target, H7_CR(bankNum), 0x00000000);
+		}
+		else {
+			LOG_ERROR("Unable to set CR register!");
+		}
+
+		/* Lock flash */
+		Lock_H7(bank, bankNum);
+	}
+	return ret;
+}
+
+static int Write_H7(struct flash_bank* bank, const uint8_t* buffer, uint32_t dstOffs, uint32_t nBytes) {
+	assert(bank != NULL);
+	assert((dstOffs + nBytes) <= bank->size);
+	uint32_t addr						= (dstOffs + bank->base);
+	assert((addr & (256/8-1)) == 0);
+	assert(bank->chip_width == 256/8);
+	int ret			= ERROR_OK;
+	if (0 < nBytes && dstOffs < (bank->size/2)) {
+		/* Write Page #0 */
+		uint32_t size	= nBytes;
+		if ((bank->size/2) < (dstOffs + size)) {
+			size		= ((bank->size/2) - dstOffs);
+		}
+		ret				= Write_H7_banked(bank, buffer, (dstOffs + bank->base), size, 0);
+		dstOffs			+= size;
+		nBytes			-= size;
+	}
+	if (ret == ERROR_OK && 0 < nBytes && (bank->size/2) <= dstOffs) {
+		/* Write Page #1 */
+		ret				= Write_H7_banked(bank, buffer, (dstOffs + bank->base), nBytes, 1);
+	}
+	return ret;
+}
+#endif
+static int Write_H7(struct flash_bank* bank, const uint8_t* buffer, uint32_t dstOffs, uint32_t nBytes) {
+	assert(bank != NULL);
+	assert((dstOffs + nBytes) <= bank->size);
+	assert((dstOffs & (256/8-1)) == 0);
+	assert(bank->chip_width == 256/8);
+	int ret			= ERROR_OK;
+
+	struct {
+		const uint8_t* Source;
+		uint32_t Addr;
+		uint32_t NBytes;
+		uint32_t Timeout;
+		bool InProgress;
+	} todo[2] = {{NULL, 0, 0, 0, false}, {NULL, 0, 0, 0, false}};
+
+	if (dstOffs < (bank->size/2)) {
+		todo[0].Source	= buffer;
+		todo[0].Addr	= (bank->base + dstOffs);
+		if ((dstOffs + nBytes) < (bank->size/2)) {
+			todo[0].NBytes	= nBytes;
+		}
+		else {
+			todo[0].NBytes	= (bank->size/2 - dstOffs);
+		}
+		todo[0].InProgress	= true;
+	}
+	if (0 < (nBytes - todo[0].NBytes)) {
+		if (dstOffs < (bank->size/2)) {
+			todo[1].Source	= (buffer + todo[0].NBytes);
+			todo[1].Addr	= (bank->base + bank->size/2);
+			todo[1].NBytes	= (nBytes - todo[0].NBytes);
+		}
+		else {
+			todo[1].Source	= buffer;
+			todo[1].Addr	= dstOffs;
+			todo[1].NBytes	= nBytes;
+		}
+		todo[1].InProgress	= true;
+	}
+
+	for (unsigned int bankNum = 0; bankNum < 2 && ret == ERROR_OK; bankNum++) {
+		if (todo[bankNum].InProgress
+			&& (ret = CheckNoOp_H7(bank, bankNum)) == ERROR_OK
+			&& (ret = ClearErrorFlags_H7(bank, bankNum)) == ERROR_OK
+			&& (ret = Unlock_H7(bank, bankNum)) == ERROR_OK
+			&& (ret = target_write_u32(bank->target, H7_CR(bankNum), 0x00000002)) == ERROR_OK) {
+			LOG_INFO("Programming bank #%u: %u bytes at 0x%08x", bankNum, todo[bankNum].NBytes, todo[bankNum].Addr);
+		}
+	}
+
+	while (todo[0].InProgress || todo[1].InProgress) {
+		for (unsigned int bankNum = 0; bankNum < 2; bankNum++) {
+			if (todo[bankNum].InProgress) {
+				ret			= Check4EOP_H7(bank, bankNum);
+				if (ret == ERROR_OK) {
+					if (0 < todo[bankNum].NBytes) {
+						uint8_t data[256/8];
+						size_t bi	= 0;
+						while (bi < sizeof(data) && 0 < todo[bankNum].NBytes) {
+							data[bi++]		= *(todo[bankNum].Source++);
+							todo[bankNum].NBytes--;
+						}
+						while (bi < sizeof(data)) {
+							data[bi++]		= 0xff;
+						}
+						if ((ret = target_write_memory(bank->target, todo[bankNum].Addr, 32/8, 256/32, data)) != ERROR_OK) {
+							LOG_ERROR("Write operation failed! 0x%08x", todo[bankNum].Addr);
+							break;
+						}
+				//		LOG_INFO("Chunk#%u  %08x", bankNum, todo[bankNum].Addr);
+						todo[bankNum].Addr		+= 256/8;
+						todo[bankNum].Timeout	= 500;
+					}
+					else {
+						todo[bankNum].InProgress	= false;
+					}
+				}
+				else if (ret == ERROR_FAIL) {
+					LOG_ERROR("Page write error!");
+					todo[bankNum].InProgress	= false;
+					break;
+				}
+				else if (todo[bankNum].Timeout == 0) {
+					LOG_ERROR("Page write timeout!");
+					todo[bankNum].InProgress	= false;
+					ret		= ERROR_FAIL;
+					break;
+				}
+				/* Timeout */
+				todo[bankNum].Timeout--;
+			}
+		}
+		alive_sleep(1);
+	}
+
+	for (unsigned int bankNum = 0; bankNum < 2; bankNum++) {
+		target_write_u32(bank->target, H7_CR(bankNum), 0x00000000);
+		Lock_H7(bank, bankNum);
+	}
+
+	return ret;
 }
 #undef H7_KEYR
 #undef H7_SR
@@ -725,6 +920,7 @@ static const struct STM32H5xxDef_s DeviceDefs[] = {
 		.FlashSize_Addr				= 0x1ff1e880,			/* RM0433, 61.2 Flash size */
 		.MassErase					= MassErase_H7,
 		.Erase						= Erase_H7,
+		.Write						= Write_H7,
 	},
 };
 
